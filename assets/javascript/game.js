a7f25eded77584a16cd564e4393355c99d0fadff
Will Salomon <40440339+KingOfPrussia2155@users.noreply.github.com>
2018-08-02 16:18:53 -0600

    more changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,24 @@
-<!DOCTYPE html>
-<html lang="en-us">
-  <head>
-    <meta charset="UTF-8">
-    <title>
-      Data Types, User Input, Conditional Logic - Commented
-    </title>
-  </head>
-  <body>
-    <script type="text/javascript">
+<script type="text/javascript">
 
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
 
-var wins = 0;
-var losses = 0; 
-var maxGuesses = 10;
-var currentGuesses = "";
+var Winner = 0;
+var Loser = 0; 
+var Remaining = 10;
+var Guesses = "";
 var randomLetter = getRandomLetter();
     
-    document.getElementById("wins").innerHTML = wins;
-    document.getElementById("losses").innerHTML = losses;
-    document.getElementById("guessesLeft").innerHTML = maxGuesses;
-    document.getElementById("guessesSoFar").innerHTML = currentGuesses;
 
-function getRandomletter {
-  var possibleLetters = "abcdefghijklmnopqrstuvwxzy"
-  return possibleLetters.charAt(Math.floor(Math.random()*possibleLetters.length));
-}
+
 
 </script>
-</body>
+
+document.getElementById("winner").innerHTML = wins;
+document.getElementById("loser").innerHTML = losses;
+document.getElementById("Remaining").innerHTML = maxGuesses;
+document.getElementById("Guesses").innerHTML = currentGuesses;
+
+function getRandomletter {
+var possibleLetters = "abcdefghijklmnopqrstuvwxzy"
+return possibleLetters.charAt(Math.floor(Math.random()*possibleLetters.length));
