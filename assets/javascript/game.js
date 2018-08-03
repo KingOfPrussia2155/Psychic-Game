27fa084735575b968716bb0b34739ab83efb068b
Will Salomon <40440339+KingOfPrussia2155@users.noreply.github.com>
2018-08-02 21:58:40 -0600

    more changes, not very tight.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,41 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-var Winner = 0;
+var Winner = 2;
 var Loser = 0; 
 var Remaining = 10;
 var Guesses = "";
 var randomLetter = getRandomLetter();
+
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); 
+    var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)]; 
+    guessesSoFar.push(userGuess); 
+    if (userGuess == computerGuess) {
+        wins++;
+        alert("Nicely played ol boy/or girl, youre out there hustlin");
+        guessesLeft = 10;
+        guessesSoFar.length = 0; 
+    }
+    else if (guessesLeft == 0){
+        losses++;
+        alert("You lose you dumbo. Its ok though I still think youre cool");
+        guessesLeft = 10;
+        guessesSoFar.length = 0;
+    }
+    else if (userGuess !== computerGuess){
+        guessesLeft--; 
+    }     
+    "<p>Total Wins: " + wins + "</p>";
+ 
+    "<p>Guesses Left: " + guessesLeft + "</p>"; 
+    "<p>Your Guesses so far: " + guessesSoFar + "</p>";
     
-
-
-
-</script>
+}
+    
+document.onkeyup = function() {
+}
 
 document.getElementById("winner").innerHTML = wins;
-document.getElementById("loser").innerHTML = losses;
+document.getElementById("Loser").innerHTML = losses;
 document.getElementById("Remaining").innerHTML = maxGuesses;
 document.getElementById("Guesses").innerHTML = currentGuesses;
 
@@ -20,4 +44,4 @@ var possibleLetters = "abcdefghijklmnopqrstuvwxzy"
 return possibleLetters.charAt(Math.floor(Math.random()*possibleLetters.length));
 
 
-document.querySelector('#game').innerHTML = html;
+document.querySelector('#game').innerHTML = html;}
